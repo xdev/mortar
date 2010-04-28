c4c0bb19de4e803dc2574d45a3928f7eb697ce50
Kenneth Love <kenneth@eyeheartzombies.com>
2010-04-28 14:06:55 -0700

    autocomplete widget

--- widgets.py
+++ widgets.py
@@ -1,10 +1,18 @@
+import os
+
+from PIL import Image
+
 from django.contrib.admin.widgets import AdminFileWidget
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 from django.conf import settings
-from PIL import Image
+from django.db.models import get_model
+from django import forms
+from django.utils import simplejson
+
 from sorl.thumbnail.base import Thumbnail
-import os
+
+from topics.models import Topic
 
 try:
   from sorl.thumbnail.main import DjangoThumbnail
@@ -23,7 +31,7 @@ class AdminImageWidget(AdminFileWidget):
   A FileField widget that displays an image instead of a file path
   if the current file is an image.
   """
-  
+
   def render(self, name, value, attrs=None):
     output = []
     file_name = str(value)
@@ -39,7 +47,37 @@ class AdminImageWidget(AdminFileWidget):
       except IOError:
         output.append('%s <a target="_blank" href="%s">%s</a> <br>%s ' % \
             (_('Currently:'), file_path, file_name, _('Change:')))
-    
+
     output.append(super(AdminFileWidget, self).render(name, value, attrs))
     return mark_safe(u''.join(output))
 
+class AutoCompleteTagInput(forms.TextInput):
+    class Media:
+        css = {
+            'all': ('css/jquery.autocomplete.css',)
+        }
+        js = (
+            'js/jquery.js',
+            'js/jquery.bgiframe.min.js',
+            'js/jquery.ajaxQueue.js',
+            'js/jquery.autocomplete.js'
+        )
+
+    def render(self, name, value, attrs=None):
+        output = super(AutoCompleteTagInput, self).render(name, value, attrs)
+        topics = Topic.objects.all()
+        topic_list = simplejson.dumps([topic.name for topic in topics],
+                                    ensure_ascii=False)
+        return output + mark_safe(u'''<script type="text/javascript">
+            jQuery("#id_%s").autocomplete(%s, {
+                width: 150,
+                max: 10,
+                highlight: false,
+                multiple: true,
+                multipleSeparator: ", ",
+                scroll: true,
+                scrollHeight: 300,
+                matchContains: true,
+                autoFill: true,
+            });
+            </script>''' % (name, topic_list))
