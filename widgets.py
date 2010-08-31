2ed9ee15050db5a4687569b1fecd85174ae63492
Charles Mastin <charles@bricksf.com>
2010-08-31 14:14:36 -0700

    Add proper remove file functionality to AdminImageWidget Add AdminFileWidget which allows removal Remove import of topic classes Remove AutoCompleteTagInput

--- widgets.py
+++ widgets.py
@@ -2,7 +2,7 @@ import os
 
 from PIL import Image
 
-from django.contrib.admin.widgets import AdminFileWidget
+from django.contrib.admin.widgets import AdminFileWidget as AdminFileWidgetBase
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 from django.conf import settings
@@ -11,8 +11,6 @@ from django import forms
 from django.utils import simplejson
 
 from sorl.thumbnail.base import Thumbnail
-
-from topics.models import Topic
 
 try:
   from sorl.thumbnail.main import DjangoThumbnail
@@ -26,12 +24,42 @@ except ImportError:
     absolute_url = os.path.join(settings.MEDIA_URL, image_path)
     return u'<img src="%s" alt="%s">' % (t.absolute_url, image_path)
 
-class AdminImageWidget(AdminFileWidget):
+class AdminFileWidget(AdminFileWidgetBase):
   """
   A FileField widget that displays an image instead of a file path
   if the current file is an image.
   """
+  
+  def __init__(self, attrs=None):
+      super(AdminFileWidget, self).__init__(attrs)
+  
+  def render(self, name, value, attrs=None):
+    output = []
+    file_name = str(value)
+    if file_name:
+      file_path = '%s/%s' % (settings.MEDIA_URL, file_name)
+      
+      output.append("""
+      <input type="checkbox" style="margin: 0;" name="remove_%s" id="id_remove_%s"/><label style="display: inline; width: 30px;" for="id_remove">Delete?</label>
+      <br>%s
+      <a target="_blank" href="%s">%s</a><br>%s<br />
+      <br />
+      """ % (name, name, _('Currently:'), file_path, file_name, _('Change:')))
+      
+    
+    output.append(super(AdminFileWidget, self).render(name, value, attrs))
+    return mark_safe(u''.join(output))
 
+
+class AdminImageWidget(AdminFileWidgetBase):
+  """
+  A FileField widget that displays an image instead of a file path
+  if the current file is an image.
+  """
+  
+  def __init__(self, attrs=None):
+      super(AdminImageWidget, self).__init__(attrs)
+  
   def render(self, name, value, attrs=None):
     output = []
     file_name = str(value)
@@ -40,44 +68,17 @@ class AdminImageWidget(AdminFileWidget):
       try:
         Image.open(os.path.join(settings.MEDIA_ROOT, file_name))
         output.append("""
+        <input type="checkbox" style="margin: 0;" name="remove_%s" id="id_remove_%s"/><label style="display: inline; width: 30px;" for="id_remove">Delete?</label>
         <a target="_blank" href="%s">%s</a><br>%s
-        <a target="_blank" href="%s">%s</a><br>%s """ % \
-            (file_path, thumbnail(file_name), _('Currently:'), file_path,
+        <a target="_blank" href="%s">%s</a><br>%s<br />
+        <br />
+        """ % \
+            (name, name, file_path, thumbnail(file_name), _('Currently:'), file_path,
               file_name, _('Change:')))
       except IOError:
         output.append('%s <a target="_blank" href="%s">%s</a> <br>%s ' % \
             (_('Currently:'), file_path, file_name, _('Change:')))
-
-    output.append(super(AdminFileWidget, self).render(name, value, attrs))
+    
+    output.append(super(AdminFileWidgetBase, self).render(name, value, attrs))
     return mark_safe(u''.join(output))
 
-class AutoCompleteTagInput(forms.TextInput):
-    class Media:
-        css = {
-            'all': ('css/jquery.autocomplete.css',)
-        }
-        js = (
-            'js/jquery.js',
-            'js/jquery.bgiframe.min.js',
-            'js/jquery.ajaxQueue.js',
-            'js/jquery.autocomplete.js'
-        )
-
-    def render(self, name, value, attrs=None):
-        output = super(AutoCompleteTagInput, self).render(name, value, attrs)
-        topics = Topic.objects.all()
-        topic_list = simplejson.dumps([topic.name for topic in topics],
-                                    ensure_ascii=False)
-        return output + mark_safe(u'''<script type="text/javascript">
-            jQuery("#id_%s").autocomplete(%s, {
-                width: 150,
-                max: 10,
-                highlight: false,
-                multiple: true,
-                multipleSeparator: ", ",
-                scroll: true,
-                scrollHeight: 300,
-                matchContains: true,
-                autoFill: true,
-            });
-            </script>''' % (name, topic_list))
